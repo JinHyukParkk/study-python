558859a5b8be98b4691338e58cde01119559068d
hyuk <hyuk@woowahan.com>
2023-12-07 01:44:54 +0900

    pandas apply 테스트

---
+++ pandas/apply.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+print(df)
+
+df = df.apply(lambda x: x ** 2)
+print(df)
+
+df = df.apply(lambda x: x ** 2, axis=1)
+print(df)
+
+
